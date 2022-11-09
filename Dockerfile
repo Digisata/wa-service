4870ddfa0ce0f3ffca3663b37966c0fea4e9ed70
Digisata <hnaufal123@gmail.com>
2022-11-09 23:53:09 +0700

    exposed endpoint

--- Dockerfile
+++ Dockerfile
@@ -2,22 +2,20 @@ FROM node:12-slim
 
 # Install Chromium.
 RUN apt-get update \
-    && apt-get install -y wget gnupg
+    && apt-get install -y wget gnupg && \
+    wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
+    echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list && \
+    apt-get update && \
+    apt-get install -y google-chrome-stable && \
+    rm -rf /var/lib/apt/lists/*s
 
-RUN \
-  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-  echo "deb http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google.list && \
-  apt-get update && \
-  apt-get install -y google-chrome-stable && \
-  rm -rf /var/lib/apt/lists/*s
-
-RUN npm i whatsapp-web.js && npm i qrcode-terminal
+RUN npm i whatsapp-web.js && npm i qrcode-terminal && npm i express && npm i express-validator
 
 RUN mkdir /app
-COPY index.js /app/index.js
+COPY app.js /app/app.js
 
 # Run everything after as non-privileged user.
 RUN useradd -ms /bin/bash wppuser
 USER wppuser
 
-CMD ["node","/app/index.js"]
+CMD ["node","/app/app.js"]
---
+++ app.js
@@ -0,0 +1,89 @@
+const qrcode = require('qrcode-terminal');
+const { Client } = require('whatsapp-web.js');
+const express = require('express');
+const { body, validationResult } = require('express-validator');
+const http = require('http');
+
+const port = process.env.PORT || 8000;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+const client = new Client({
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox']
+    }
+});
+
+client.on('qr', qr => {
+    qrcode.generate(qr, { small: true });
+});
+
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
+
+client.on('message', message => {
+    if (message.body === '!ping') {
+        message.reply('pong');
+    }
+});
+
+client.initialize();
+
+const phoneNumberFormatter = function (number) {
+    let formatted = number.replace(/\D/g, '');
+
+    if (formatted.startsWith('0')) {
+        formatted = '62' + formatted.substr(1);
+    }
+
+    if (!formatted.endsWith('@c.us')) {
+        formatted += '@c.us';
+    }
+
+    return formatted;
+}
+
+// Send message
+app.post('/send-message', [
+    body('number').notEmpty(),
+    body('message').notEmpty(),
+], async (req, res) => {
+    const errors = validationResult(req).formatWith(({
+        msg
+    }) => {
+        return msg;
+    });
+
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            status: false,
+            message: errors.mapped()
+        });
+    }
+
+    const number = phoneNumberFormatter(req.body.number);
+    const message = req.body.message;
+
+    client.sendMessage(number, message).then(response => {
+        res.status(200).json({
+            status: true,
+            response: response
+        });
+    }).catch(err => {
+        res.status(500).json({
+            status: false,
+            response: err
+        });
+    });
+});
+
+server.listen(port, function () {
+    console.log('App running on *: ' + port);
+});
