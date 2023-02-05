3b01538131f04ee7e3f6491108148cb74b9fdc20
Digisata <hnaufal123@gmail.com>
2023-02-05 14:58:38 +0700

    added event handler

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
-const qrcode = require('qrcode-terminal');
-const { Client } = require('whatsapp-web.js');
-const express = require('express');
-const { body, validationResult } = require('express-validator');
-const http = require('http');
+const qrcode = require("qrcode-terminal");
+const { Client } = require("whatsapp-web.js");
+const express = require("express");
+const { body, validationResult } = require("express-validator");
+const http = require("http");
 
 const port = process.env.PORT || 8000;
 
@@ -10,80 +10,98 @@ const app = express();
 const server = http.createServer(app);
 
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
 const client = new Client({
-    puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+  puppeteer: {
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  },
+});
+
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
+
+client.on("ready", () => {
+  console.log("Client is ready!");
+});
+
+client.on("message", (message) => {
+  if (message.body === "!ping") {
+    message.reply("pong");
+  }
+});
+
+let myVar;
+client.on("message_create", (message) => {
+  if (message.body === "!spam") {
+    myVar = setInterval(myTimer, 300);
+    function myTimer() {
+      client.sendMessage(message.to, "😞");
     }
-});
+  }
 
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
-});
-
-client.on('ready', () => {
-    console.log('Client is ready!');
-});
-
-client.on('message', message => {
-    if (message.body === '!ping') {
-        message.reply('pong');
-    }
+  if (message.body === "!stop") {
+    clearInterval(myVar);
+  }
 });
 
 client.initialize();
 
 const phoneNumberFormatter = function (number) {
-    let formatted = number.replace(/\D/g, '');
+  let formatted = number.replace(/\D/g, "");
 
-    if (formatted.startsWith('0')) {
-        formatted = '62' + formatted.substr(1);
-    }
+  if (formatted.startsWith("0")) {
+    formatted = "62" + formatted.substr(1);
+  }
 
-    if (!formatted.endsWith('@c.us')) {
-        formatted += '@c.us';
-    }
+  if (!formatted.endsWith("@c.us")) {
+    formatted += "@c.us";
+  }
 
-    return formatted;
-}
+  return formatted;
+};
 
 // Send message
-app.post('/send-message', [
-    body('number').notEmpty(),
-    body('message').notEmpty(),
-], async (req, res) => {
-    const errors = validationResult(req).formatWith(({
-        msg
-    }) => {
-        return msg;
+app.post(
+  "/send-message",
+  [body("number").notEmpty(), body("message").notEmpty()],
+  async (req, res) => {
+    const errors = validationResult(req).formatWith(({ msg }) => {
+      return msg;
     });
 
     if (!errors.isEmpty()) {
-        return res.status(422).json({
-            status: false,
-            message: errors.mapped()
-        });
+      return res.status(422).json({
+        status: false,
+        message: errors.mapped(),
+      });
     }
 
     const number = phoneNumberFormatter(req.body.number);
     const message = req.body.message;
 
-    client.sendMessage(number, message).then(response => {
+    client
+      .sendMessage(number, message)
+      .then((response) => {
         res.status(200).json({
-            status: true,
-            response: response
+          status: true,
+          response: response,
         });
-    }).catch(err => {
+      })
+      .catch((err) => {
         res.status(500).json({
-            status: false,
-            response: err
+          status: false,
+          response: err,
         });
-    });
-});
+      });
+  }
+);
 
 server.listen(port, function () {
-    console.log('App running on *: ' + port);
+  console.log("App running on *: " + port);
 });
